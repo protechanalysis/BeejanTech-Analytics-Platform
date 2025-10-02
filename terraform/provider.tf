c5ceafa1ba9917204843acda6c81642f5f9d42f9
protechanalysis <adewunmioluwaseyi98@.com>
2025-10-02 12:47:17 +0100

    testing ci/cd

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,7 @@
 terraform {
-  required_providers {
-    required_version = "1.13.3"
+  required_version = "=>1.13.3"
 
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~>6.5.0"
