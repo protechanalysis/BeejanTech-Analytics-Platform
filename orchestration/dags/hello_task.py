bfa4886f76e2cf323c4e93be410d2800404b1a1a
Adewunmi Olaniyi Oluwaseyi <108165968+protechanalysis@users.noreply.github.com>
2025-10-13 19:20:17 +0100

    Dev2 (#38)
    dags change

--- orchestration/dags/hello_task.py
+++ orchestration/dags/hello_task.py
@@ -3,7 +3,7 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 
 def print_hello():
-    for i in range(1, 51):  # Loop from 1 to 50
+    for i in range(1, 9):  # Loop from 1 to 50
         print(f"Hello {i}")
 
 # Define DAG
